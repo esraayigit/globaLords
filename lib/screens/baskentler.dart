559024eecbaa8c81a37d27785c3daffd7f5f89c7
busrabetus <poslubusrab@gmail.com>
2025-04-02 17:59:56 +0300

    baskent sayfasina dogru yanlis cevap efekti eklendi

--- lib/screens/baskentler.dart
+++ lib/screens/baskentler.dart
@@ -15,21 +15,21 @@ class _BaskentlerState extends State<Baskentler> {
   int currentQuestionIndex = 0;
   bool hintUsed = false;
   TextEditingController answerController = TextEditingController();
+  bool isAnswerCorrect = false;
+  bool isAnswered = false;
 
   final List<Map<String, dynamic>> questions = [
-    {"image": "assets/images/baskentler/berlin.jpg", "answer": "Almanya", "hint": "Bu ülke ünlü otobüs markası Volkswagen'e sahiptir."},
-    {"image": "assets/images/baskentler/tokyo.jpg", "answer": "Japonya", "hint": "Bu ülke, geleneksel çay seremonisi ve samuray kültürüyle tanınır."},
-    {"image": "assets/images/baskentler/belgrad.jpeg", "answer": "Sırbistan", "hint": "Bu ülke, tarihi Yugoslavya'nın bir parçasıydı ve Balkanlar'da yer alır."},
-    {"image": "assets/images/baskentler/bern.jpg", "answer": "İsviçre", "hint": "Bu ülke, çikolata ve saat üretimiyle ünlüdür."},
-    {"image": "assets/images/baskentler/moscow.jpeg", "answer": "Rusya", "hint": "Bu ülke, dünyanın en büyük kara sınırına sahip olup, ünlü Matruşka bebekleriyle tanınır."},
-    {"image": "assets/images/baskentler/canberra.jpg", "answer": "Avustralya", "hint": "Bu ülke, kangurular, koalalar ve Büyük Set Resifi ile ünlüdür."},
-    {"image": "assets/images/baskentler/kiev.jpeg", "answer": "Ukrayna", "hint": "Bu ülke, dünyanın en büyük tahıl üreticilerinden biridir."},
-    {"image": "assets/images/baskentler/kopenhag.jpg", "answer": "Danimarka", "hint": "Bu ülke, mutlu insanlar ve Lego'nun doğum yeri olarak bilinir."},
-    {"image": "assets/images/baskentler/sofya.jpeg", "answer": "Bulgaristan", "hint": "Bu ülke, geleneksel yoğurt üretimi ve eski Roma kalıntılarıyla tanınır."},
-    {"image": "assets/images/baskentler/wellington.jpg", "answer": "Yeni Zelanda", "hint": "Bu ülke, Hobbit ve Yüzüklerin Efendisi film serilerinin çekildiği yerdir."},
+    {"image": "assets/images/baskentler/berlin.jpg","baskent": "Berlin", "answer": "Almanya", "hint": "Bu ülke ünlü otobüs markası Volkswagen'e sahiptir."},
+    {"image": "assets/images/baskentler/tokyo.jpg","baskent": "Tokyo", "answer": "Japonya", "hint": "Bu ülke, geleneksel çay seremonisi ve samuray kültürüyle tanınır."},
+    {"image": "assets/images/baskentler/belgrad.jpeg","baskent": "Belgrad", "answer": "Sirbistan", "hint": "Bu ülke, tarihi Yugoslavya'nın bir parçasıydı ve Balkanlar'da yer alır."},
+    {"image": "assets/images/baskentler/bern.jpg","baskent": "Bern", "answer": "İsvicre", "hint": "Bu ülke, çikolata ve saat üretimiyle ünlüdür."},
+    {"image": "assets/images/baskentler/moscow.jpeg","baskent": "Moskova", "answer": "Rusya", "hint": "Bu ülke, dünyanın en büyük kara sınırına sahip olup, ünlü Matruşka bebekleriyle tanınır."},
+    {"image": "assets/images/baskentler/canberra.jpg","baskent": "Canberra", "answer": "Avustralya", "hint": "Bu ülke, kangurular, koalalar ve Büyük Set Resifi ile ünlüdür."},
+    {"image": "assets/images/baskentler/kiev.jpeg","baskent": "Kiev", "answer": "Ukrayna", "hint": "Bu ülke, dünyanın en büyük tahıl üreticilerinden biridir."},
+    {"image": "assets/images/baskentler/kopenhag.jpg","baskent": "Kopenhag", "answer": "Danimarka", "hint": "Bu ülke, mutlu insanlar ve Lego'nun doğum yeri olarak bilinir."},
+    {"image": "assets/images/baskentler/sofya.jpeg","baskent": "Sofya", "answer": "Bulgaristan", "hint": "Bu ülke, geleneksel yoğurt üretimi ve eski Roma kalıntılarıyla tanınır."},
+    {"image": "assets/images/baskentler/wellington.jpg","baskent": "Wellington", "answer": "Yeni Zelanda", "hint": "Bu ülke, Hobbit ve Yüzüklerin Efendisi film serilerinin çekildiği yerdir."},
   ];
-
-
 
   @override
   void initState() {
@@ -57,8 +57,20 @@ class _BaskentlerState extends State<Baskentler> {
         title: Text("Süreniz Doldu!"),
         content: Text("Toplam Puanınız: $score\nTekrar denemek ister misiniz?"),
         actions: [
-          TextButton(onPressed: () => Navigator.pop(context), child: Text("Ana Sayfa")),
-          TextButton(onPressed: restartGame, child: Text("Tekrar Dene")),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, '/homepage');
+            },
+            child: Text("Ana Sayfa"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              restartGame();
+            },
+            child: Text("Tekrar Dene"),
+          ),
         ],
       ),
     );
@@ -76,22 +88,26 @@ class _BaskentlerState extends State<Baskentler> {
   }
 
   void checkAnswer() {
+
     String userAnswer = answerController.text.trim().toLowerCase();
     String correctAnswer = questions[currentQuestionIndex]["answer"].toString().toLowerCase();
 
-    if (userAnswer == correctAnswer) {
-      setState(() {
+    setState(() {
+      if (userAnswer == correctAnswer) {
         score += 20;
-      });
-    }
-    moveToNextQuestion();
+        isAnswerCorrect = true;
+      } else {
+        isAnswerCorrect = false;
+      }
+      isAnswered = true;
+    });
   }
 
   void moveToNextQuestion() {
     if (currentQuestionIndex < questions.length - 1) {
       setState(() {
         currentQuestionIndex++;
-        hintUsed = false;
+        isAnswered = false;
         answerController.clear();
       });
     } else {
@@ -107,7 +123,13 @@ class _BaskentlerState extends State<Baskentler> {
         title: Text("Oyun Bitti!"),
         content: Text("Toplam Puanınız: $score"),
         actions: [
-          TextButton(onPressed: restartGame, child: Text("Tekrar Oyna")),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              restartGame();
+            },
+            child: Text("Tekrar Dene"),
+          ),
         ],
       ),
     );
@@ -157,25 +179,55 @@ class _BaskentlerState extends State<Baskentler> {
             Card(
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
               elevation: 4,
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
-                    child: Image.asset(
-                      questions[currentQuestionIndex]["image"],
-                      height: 250,
-                      width: 300,
-                      fit: BoxFit.cover,
+              child: SizedBox(
+                height: 350,
+                width: 350,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      child: isAnswered
+                          ? Center(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              isAnswerCorrect ? Icons.check_circle : Icons.cancel,
+                              color: isAnswerCorrect ? Colors.green : Colors.red,
+                              size: 24,
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              isAnswerCorrect ? 'Doğru Cevap' : 'Yanlış Cevap',
+                              style: TextStyle(
+                                color: isAnswerCorrect ? Colors.green : Colors.red,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                          : null,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      questions[currentQuestionIndex]["answer"],
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ClipRRect(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+                      child: Image.asset(
+                        questions[currentQuestionIndex]["image"],
+                        height: 250,
+                        width: 300,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        questions[currentQuestionIndex]["baskent"],
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             SizedBox(height: 20),
@@ -190,6 +242,14 @@ class _BaskentlerState extends State<Baskentler> {
             ElevatedButton(onPressed: checkAnswer, child: Text("Cevapla")),
             ElevatedButton(onPressed: useHint, child: Text("İpucu al (-10 puan)")),
             if (hintUsed) Text(questions[currentQuestionIndex]["hint"], style: TextStyle(color: Colors.red)),
+            ElevatedButton(
+              onPressed: isAnswered
+                  ? () {
+                moveToNextQuestion();
+              }
+                  : null,
+              child: Text("Diğer Soruyu Geç"),
+            ),
           ],
         ),
       ),
